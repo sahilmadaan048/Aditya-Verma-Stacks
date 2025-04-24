493d7d1394b2914d4dcfec268f858245b6e30aac
Sahil Madaan <madaan.sahil27@gmail.com>
2025-04-24 23:44:31 +0530

    stack span problem

---
+++ 6. Stack Span Problem/main.cpp
@@ -0,0 +1,92 @@
+// https://www.youtube.com/watch?v=p9T-fE1g1pU&list=PLwEApySc2G_k4Zl1gGgQdC2NfykPKCMwR&index=6
+
+/*
+
+The stock span problem is a financial problem where we have a series of n daily price quotes for a stock and we need to calculate span of stock’s price for all n days.
+The span Si of the stock’s price on a given day i is defined as the maximum number of consecutive days just before the given day, for which the price of the stock on the current day is less than or equal to its price on the given day.
+For example, if an array of 7 days prices is given as {100, 80, 60, 70, 60, 75, 85}, then the span values for corresponding 7 days are {1, 1, 1, 2, 1, 4, 6}
+
+*/
+
+#include "bits/stdc++.h"
+#define int long long
+#define uint unsigned long long
+#define vi vector<int>
+#define vvi vector<vi >
+#define vb vector<bool>
+#define vvb vector<vb >
+#define fr(i,n) for(int i=0; i<(n); i++)
+#define rep(i,a,n) for(int i=(a); i<=(n); i++)
+#define nl cout<<"\n"
+#define dbg(var) cout<<#var<<"="<<var<<" "
+#define all(v) v.begin(),v.end()
+#define sz(v) (int)(v.size())
+#define srt(v)  sort(v.begin(),v.end())         // sort 
+#define mxe(v)  *max_element(v.begin(),v.end())     // find max element in vector
+#define mne(v)  *min_element(v.begin(),v.end())     // find min element in vector
+#define unq(v)  v.resize(distance(v.begin(), unique(v.begin(), v.end())));
+// make sure to sort before applying unique // else only consecutive duplicates would be removed 
+#define bin(x,y)  bitset<y>(x) 
+using namespace std;
+int MOD=1e9+7;      // Hardcoded, directly change from here for functions!
+
+
+
+void modadd(int &a , int b) {a=((a%MOD)+(b%MOD))%MOD;}
+void modsub(int &a , int b) {a=((a%MOD)-(b%MOD)+MOD)%MOD;}
+void modmul(int &a , int b) {a=((a%MOD)*(b%MOD))%MOD;}
+// ================================== take ip/op like vector,pairs directly!==================================
+template<typename typC,typename typD> istream &operator>>(istream &cin,pair<typC,typD> &a) { return cin>>a.first>>a.second; }
+template<typename typC> istream &operator>>(istream &cin,vector<typC> &a) { for (auto &x:a) cin>>x; return cin; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const pair<typC,typD> &a) { return cout<<a.first<<' '<<a.second; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const vector<pair<typC,typD>> &a) { for (auto &x:a) cout<<x<<'\n'; return cout; }
+template<typename typC> ostream &operator<<(ostream &cout,const vector<typC> &a) { int n=a.size(); if (!n) return cout; cout<<a[0]; for (int i=1; i<n; i++) cout<<' '<<a[i]; return cout; }
+// ===================================END Of the input module ==========================================
+
+//consecutive <= before that day(index)
+//finding the nearest greater on left basically
+
+
+void solve() {
+    vector<int> arr = {100, 80, 60, 70, 60, 75, 85};
+    int n = arr.size();
+    vector<int> span(n); // to store the span for each day
+    stack<int> st; // to store indices
+
+    for (int i = 0; i < n; ++i) {
+        // Pop elements from the stack while stack is not empty and
+        // the price at top of stack is less than or equal to current price
+        while (!st.empty() && arr[st.top()] <= arr[i]) {
+            st.pop();
+        }
+
+        // If stack is empty, it means no greater element on left
+        span[i] = st.empty() ? (i + 1) : (i - st.top());
+
+        // Push this element's index to stack
+        st.push(i);
+    }
+
+    // Output the result
+    for (int s : span) {
+        cout << s << " ";
+    }
+    cout << endl;
+}
+
+int32_t main()
+{
+ 
+ ios_base::sync_with_stdio(false);
+ cin.tie(NULL);
+
+    int T = 1;
+    // cin >> T;
+    while (T--)
+    {
+        solve();
+    }
+    return 0;
+}
+
+    
